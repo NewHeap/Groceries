14f6d2081305f86d49ebac29a43dc4ddb2fa2905
MrKing95 <thomas@newheap.com>
2017-09-26 10:22:37 +0200

    examples
    awd

---
+++ Groceries/DAL/Repositories/Repository.cs
@@ -0,0 +1,35 @@
+﻿using GroceriesTool.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GroceriesTool.DAL.Repositories
+{
+    public abstract class Repository<T> : IRepository<T> where T : class, IDb
+    {
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Find(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
